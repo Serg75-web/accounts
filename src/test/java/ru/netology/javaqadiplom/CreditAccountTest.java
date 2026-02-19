13089561e5006921497b2ad58125cff8cb69712e
Serg75 <activequest@gmail.com>
2026-02-19 15:52:42 +0300

    tests

--- src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
@@ -19,55 +19,5 @@ public class CreditAccountTest {
 
         Assertions.assertEquals(3_000, account.getBalance());
     }
-
-    @Test
-    public void negativeRate() {
-        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
-            new CreditAccount(0, 5_000, -10);
-        });
-
-        String expectedMessage = "Накопительная ставка не может быть отрицательной, а у вас: -10";
-        String actualMessage = exception.getMessage();
-
-        Assertions.assertEquals(expectedMessage, actualMessage);
-    }
-
-    @Test
-    public void shouldAddToNegativeBalance() {
-        CreditAccount account = new CreditAccount(
-                -1_000,
-                5_000,
-                15
-        );
-
-        account.add(3_000);
-
-        Assertions.assertEquals(3_000, account.getBalance());
-    }
-
-    @Test
-    public void shouldAddToNegativeCreditLimit() {
-        CreditAccount account = new CreditAccount(
-                0,
-                -1_000,
-                15
-        );
-
-        account.add(3_000);
-
-        Assertions.assertEquals(3_000, account.getBalance());
-    }
-
-//    @Test
-//    public void negativeRate1() {
-//        CreditAccount account = new CreditAccount(
-//                0,
-//                5_000,
-//                -15
-//        );
-//
-//        account.add(3_000);
-//
-//        Assertions.assertEquals(3_000, account.getBalance());
-//    }
+    
 }
