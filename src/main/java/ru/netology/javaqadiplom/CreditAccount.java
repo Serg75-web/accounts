8d7273fed6fbe32de0dc0da9497ad054c51ca99f
Serg75 <activequest@gmail.com>
2026-02-20 17:25:43 +0300

    methodsCorrection

--- src/main/java/ru/netology/javaqadiplom/CreditAccount.java
+++ src/main/java/ru/netology/javaqadiplom/CreditAccount.java
@@ -66,9 +66,10 @@ public class CreditAccount extends Account {
         if (amount <= 0) {
             return false;
         }
-        balance = amount;
+        balance += amount; // увеличиваем баланс на сумму пополнения
         return true;
     }
+
 
     /**
      * Операция расчёта процентов на отрицательный баланс счёта при условии, что
